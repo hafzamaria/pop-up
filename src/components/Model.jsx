04b432883c52ecc241b826ee964e5bc4e3941776
mariadhantia <mariadhantia@gmail.com>
2023-01-20 02:06:31 +0500

    pop-upWithoutScroll

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import MyModel from './MyModel';
 
 const Model = () => {
+
     const [showModel, setShowModel] = useState(false);
 
     const closeModel=()=>{
@@ -13,7 +14,61 @@ const Model = () => {
     
         <button className='modal-btn' onClick={()=>setShowModel(true)}>Open Modal</button>
         {showModel && <MyModel closeModel={closeModel}/>}
-       
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Fuga est magnam ex praesentium obcaecati ipsum odio provident temporibus, 
+             amet error aut modi numquam accusamus expedita quae repellendus repudiandae, animi quo!</p>
+             <br/>
+             <br/>
      
       </>
     )
--- src/components/MyModel.jsx
+++ src/components/MyModel.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { useEffect } from 'react';
 
 const MyModel=({closeModel})=>{
-   
+    /////agr hm pop-up k sath scrol ni chate
+    useEffect(()=>{
+        document.body.style.overflowY="hidden";
+        return()=>{
+            document.body.style.overflowY="scroll";
+        };
+    
+    },[]);
     return(
       <>
       {/* //model-wrapper div pe onclick se page pe kahi b clik karege tu pop-up close ho jaega */}
